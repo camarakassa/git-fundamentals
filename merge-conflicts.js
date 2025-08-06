bae07abdc4cc33963ced669b71c27600fe40c794
manga-kassa <kassa.camara@univ-thies.sn>
2025-08-05 21:08:30 -0400

    improve syntax

--- merge-conflicts.js
+++ merge-conflicts.js
@@ -1,13 +1,9 @@
  // This function adds 2 nums
-const add = (a, b) => {
-  return a + b;
-};
+const add = (a, b) =>  a + b;
 
-const substract = (a, b) => {
-  return a - b
-}
+const subtract = (a, b) => a - b;
 
 export {
   add,
-  substract
-}
+  subtract
+};
