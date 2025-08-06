5b10afbb014453cb2bb383edb9f4737d33aa5ab9
manga-kassa <kassa.camara@univ-thies.sn>
2025-08-05 21:22:24 -0400

    improve divide function and export it

--- merge-conflicts.js
+++ merge-conflicts.js
@@ -4,10 +4,15 @@ const add = (a, b) =>  a + b;
 const subtract = (a, b) => a - b;
 
 const divide = (a, b) => {
+if (b === 0){
+ throw Error("Cannot divide by 0");
+}
  return a / b;
 }
+ 
 export {
   add,
-  subtract
+  subtract,
+  divde
 };
 }
